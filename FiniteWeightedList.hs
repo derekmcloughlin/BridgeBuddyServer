7c21152316f13f22fb06dbc8559c1bc0fe1f90c1
Derek McLoughlin <derek.mcloughlin@gmail.com>
2014-01-05 12:10:34 +0000

    Added comments

--- FiniteWeightedList.hs
+++ FiniteWeightedList.hs
@@ -1,10 +1,41 @@
--- Finite Weighted List
--- 
+{-
+Finite Weighted List
+This data structure stores at most N items grouped by a string, with weighted
+distribution of each group.
+
+For example: if we want to store a bridge hand and it's bid but ensure that we have
+a certain distribution of 'rare' bids (e.g. 3C), then we could use this:
+
+    bridgeHands :: FiniteWeightedList Hand
+    bridgeHands  = FiniteWeightedList {
+        weights = Map.fromList [
+            ("1C",   9), 
+            ("1D",   9),
+            ("1H",   9),
+            ("1S",   9),
+            ("1NT",  9),
+            ("2NT",  9),
+            ("2C",   9),
+            ("3C",   9),
+            ("3D",   9),
+            ("3H",   9),
+            ("3S",   9),
+            ("PASS", 9)
+            ],
+        counts = Map.empty,
+        items = [],
+        maxItems = 1000
+    }
+
+When populating the list the commonly-occurring bids (1C - 1S) will fill up quickly. Once they've
+reached their quota, the rarer bids will fill the rest of the list. Although the list will have
+the correct distribution, it won't be random, so you should shuffle the result afterwards if desired.
+
+-}
 
 module FiniteWeightedList where
 
 import qualified Data.Map as Map
-import System.Random.Shuffle
 import Data.Maybe
 
 data FiniteWeightedList a = 
@@ -15,10 +46,11 @@ data FiniteWeightedList a =
         maxItems :: Int
     }
 
+-- Add a single item to the list
 addItem :: FiniteWeightedList a -> String -> a -> FiniteWeightedList a
 addItem fwl key item 
     | total_count == maxItems fwl      = fwl   -- List is completely full, don't add any more
-    | current_item_pc >= target_item_pc = fwl   -- List has reached it's percentage quota of 'item' - don't add any more
+    | current_item_pc >= target_item_pc = fwl  -- List has reached it's percentage quota of 'item' - don't add any more
     | otherwise = FiniteWeightedList {
             weights = weights fwl,
             counts = Map.insert key (num_items + 1) (counts fwl),
@@ -33,27 +65,8 @@ addItem fwl key item
                 | otherwise         = (100 * num_items) `div` total_count
             get_map_item k m        = fromMaybe 0 (Map.lookup k m)
 
+-- Add a list of items to the data structure.
+-- A function 'f' that gives us the group string is provided.
 addList :: FiniteWeightedList a -> [a] -> (a -> String) -> FiniteWeightedList a
 addList fwl xs f = foldl (\ fwl x -> addItem fwl (f x) x) fwl xs
 
--- Example 
-myList :: FiniteWeightedList String
-myList = FiniteWeightedList {
-    weights = Map.fromList [
-        ("1C",   9), 
-        ("1D",   9),
-        ("1H",   9),
-        ("1S",   9),
-        ("1NT",  9),
-        ("2NT",  9),
-        ("2C",   9),
-        ("3C",   9),
-        ("3D",   9),
-        ("3H",   9),
-        ("3S",   9),
-        ("PASS", 9)
-        ],
-    counts = Map.empty,
-    items = [],
-    maxItems = 1000
-}
--- README.md
+++ README.md
@@ -4,7 +4,12 @@ BridgeBuddyServer
 A JSON RPC Server for Bridge Buddy
 
 BridgeBuddyServer is the back-end for BridgeBuddy, an Android and iOS app for helping those
-learning the card game Bridge, especially in bidding and leads.
+learning the card game Bridge, especially in ACOL bidding and leads.
+
+Spec
+----
+
+The ACOL bidding system used is described in the [No Fear Bridge Crib Sheet](http://www.nofearbridge.co.uk/crib_sheet_new.pdf)
 
 Setting Up
 ----------
